a5a0417c9b1425c56ddaa409f93da0bf1729e123
Alexdieu <alexdieuveult@gmail.com>
2025-11-29 12:51:22 +0100

    Update interpreter.py
    Ptit refactor pour plus de robustess

--- interpreter.py
+++ interpreter.py
@@ -1,261 +1,186 @@
 import sys
-import os.path
+import os
 import time
-import re
+import operator
 
-print("STALINIUM V1 PAR ALEXDIEU")
+class StaliniumInterpreter:
+    def __init__(self):
+        self.variables = {}
+        self.running = True
+        self.ops = {
+            "+": operator.add, "-": operator.sub, "*": operator.mul,
+            "/": operator.truediv, "**": operator.pow, "//": operator.mod,
+            "%": operator.mod,
+        }
+        self.comparators = {
+            "?=": operator.eq, "!=": operator.ne, "<": operator.lt,
+            ">": operator.gt, "<=": operator.le, ">=": operator.ge
+        }
 
-if len(sys.argv) > 1:
-    file = sys.argv[1]
-    if not os.path.isfile(file):
-        print("Error, File doesn't exist!")
-        sys.exit(1)
-else:
-    file = input(">>>")
+    def normalize_var_name(self, name_parts):
+        """Converts ['my', 'var'] to '-my-var' to match original behavior."""
+        return "-" + "-".join(name_parts)
 
-variables = {}
+    def resolve_variables(self, text):
+        """Replaces variables in a string with their values."""
+        if ":" not in text:
+            return text
 
-def lts(a):
-    return ' '.join(a)
-
-def CHEZCFIRST(mot, ligne):
-    prem = ligne.partition(mot)[0]
-    if prem.isspace() or prem == '':
-        return True
-    else:
-        return False
-
-def calc(ligne: list) -> str:
-    try:
-        op = ligne[2]
-        left = float(ligne[1])
-        right = float(ligne[3])
-        if op == "+":
-            return str(left + right)
-        elif op == "-":
-            return str(left - right)
-        elif op == "*":
-            return str(left * right)
-        elif op == "/":
-            return str(left / right)
-        elif op == "**":
-            return str(left ** right)
-        elif op == "//":
-            return str(left % right)  # Note: Changed to % as per original intent, but // typically floor div
-        else:
-            return check_if(ligne)
-    except IndexError:
-        try:
-            expr = ligne[1]
-            if "+" in expr:
-                prems, _, sec = expr.partition("+")
-                ope = "+"
-            elif "-" in expr:
-                prems, _, sec = expr.partition("-")
-                ope = "-"
-            elif "*" in expr:
-                prems, _, sec = expr.partition("*")
-                ope = "*"
-            elif "/" in expr:
-                prems, _, sec = expr.partition("/")
-                ope = "/"
-            elif "**" in expr:
-                prems, _, sec = expr.partition("**")
-                ope = "**"
-            elif "//" in expr:
-                prems, _, sec = expr.partition("//")
-                ope = "//"
+        words = text.split()
+        resolved_words = []
+        i = 0
+        while i < len(words):
+            word = words[i]
+            if word.startswith(":") and not word.startswith("\\:"):
+                match_found = False
+                for length in range(len(words) - i, 0, -1):
+                    potential_name_parts = words[i:i+length]
+                    potential_name_parts[0] = potential_name_parts[0][1:]
+                    key = self.normalize_var_name(potential_name_parts)
+                    
+                    if key in self.variables:
+                        resolved_words.append(str(self.variables[key]))
+                        i += length
+                        match_found = True
+                        break
+                
+                if not match_found:
+                    resolved_words.append(word)
+                    i += 1
             else:
-                return "ERREUR 2, Opérateur inconnu ! : " + ' '.join(ligne)
-            return "ERREUR 1, les nombres doivent être espacés de l'opérateur comme ça : " + prems + " " + ope + " " + sec
-        except:
+                resolved_words.append(word.replace("\\:", ":"))
+                i += 1
+        return " ".join(resolved_words)
+
+    def cmd_montre(self, args):
+        text = " ".join(args)
+        print(self.resolve_variables(text))
+
+    def cmd_calcul(self, args):
+        if len(args) < 3:
             return "ERREUR 7 : AUCUN CALCUL DONNE"
-    except ValueError:
-        return "ERREUR 4 : Valeurs non numériques pour l'opération"
+        try:
+            left, op, right = float(args[0]), args[1], float(args[2])
+            if op in self.ops:
+                return str(self.ops[op](left, right))
+            return f"ERREUR 2, Opérateur inconnu ! : {op}"
+        except ValueError:
+            return "ERREUR 4 : Valeurs non numériques"
+        except ZeroDivisionError:
+            return "ERREUR: Division par zéro"
 
-def check_if(ligne: list) -> str:
-    args = ligne[1:]
-    if len(args) != 3:
-        return "ERREUR 7 AUCUNE CONDITION DONNE"
-    try:
-        left = float(args[0])
-        right = float(args[2])
-        op = args[1]
-        if op == "?=":
-            return "Vrai" if left == right else "Faux"
-        elif op == "<":
-            return "Vrai" if left < right else "Faux"
-        elif op == ">":
-            return "Vrai" if left > right else "Faux"
-        elif op == "<=":
-            return "Vrai" if left <= right else "Faux"
-        elif op == ">=":
-            return "Vrai" if left >= right else "Faux"
-        elif op == "!=":
-            return "Vrai" if left != right else "Faux"
-        else:
-            return "ERREUR 2, Opérateur inconnu ! : " + op
-    except ValueError:
-        left = args[0]
-        right = args[2]
-        op = args[1]
-        if op == "?=":
-            return "Vrai" if left == right else "Faux"
-        elif op == "!=":
-            return "Vrai" if left != right else "Faux"
-        else:
-            return "ERREUR 4 : Opérateur de comparaison nécessite des nombres ou ?= / !="
-
-def scan(lignes: list):
-    for ligne in lignes:
-        bon = False
-        split_ligne = ligne.split()
-        if CHEZCFIRST("montre", ligne):
-            lit = " ".join(split_ligne[1:])
-            if ":" in lit:
-                check = lit.partition(":")[0]
-                if check and check[-1] == "\\":
-                    pass
-                else:
-                    erreurs = 0
-                    erreur = []
-                    erN = []
-                    name = lit.partition(":")[2].split()
-                    NAME = ''
-                    NAMEV = ''
-                    ende = len(name)
-                    for i in range(ende):
-                        NAME += '-' + name[i]
-                        if i == 0:
-                            NAMEV = ':' + name[i]
-                        else:
-                            NAMEV += ' ' + name[i]
-                        if NAME in variables:
-                            erreurs += 1
-                            erreur.append(NAME)
-                            erN.append(NAMEV)
-                    ni = False
-                    if erreurs == 0:
-                        ni = True
-                        print("ERREUR 6 : VARIABLE N'EXISTE PAS")
-                    if erreurs == 1:
-                        ni = True
-                        val = variables[erreur[0]]
-                        lit = lit.replace(erN[0], val, 1)
-                    if erreurs == 2:
-                        ni = True
-                        print("ATTENTION : DEUX VARIABLES ONT UN NOM SIMILIAIRE AU DEBUT : " + erN[0] + " et " + erN[1] + " ! STALINIUM prends la première par défault ! ")
-                        val = variables[erreur[0]]
-                        lit = lit.replace(erN[0], val, 1)
-                    elif erreurs > 2:
-                        ni = True
-                        print("ATTENTION : PLUSIEURS VARIABLES ONT UN NOM SIMILIAIRE AU DEBUT ! STALINIUM prends la première par défault ! ")
-                        val = variables[erreur[0]]
-                        lit = lit.replace(erN[0], val, 1)
-            if "\\:" in lit:
-                lit = lit.replace("\\:", ":")
-            print(lit)
-            bon = True
-        if CHEZCFIRST("calcul", ligne):
-            print(calc(split_ligne))
-            bon = True
-        if CHEZCFIRST("si", ligne):
-            print(check_if(split_ligne))
-            bon = True
-        if CHEZCFIRST("sortir", ligne):
-            prem = ligne.partition("sortir")[0]
-            sec = ligne.partition("sortir")[2]
-            if (prem.isspace() or prem == '') and (sec.isspace() or sec == ''):
-                sys.exit(0)
-        if CHEZCFIRST("dors", ligne):
+    def cmd_si(self, args):
+        if len(args) != 3:
+            return "ERREUR 7 AUCUNE CONDITION DONNE"
+        try:
+            left, op, right = args[0], args[1], args[2]
             try:
-                bon_sleep = int(split_ligne[1])
-                listDORS = split_ligne.copy()
-                listDORS.remove("dors")
-                listDORS.remove(split_ligne[1])
-                arg = lts(listDORS)
-                if arg:
-                    print(arg)
-                time.sleep(bon_sleep)
-                bon = True
-            except (IndexError, ValueError):
-                print("ERREUR 3 : Mauvaise SYNTAXE pour DORS : DORS + TEMPS // exemple : dors 2")
-        if CHEZCFIRST("pause", ligne):
-            sec = ligne.partition("pause")[2].lstrip()
-            if sec:
-                input(sec + "...")
+                left = float(left)
+                right = float(right)
+            except ValueError:
+                pass 
+                
+            if op in self.comparators:
+                return "Vrai" if self.comparators[op](left, right) else "Faux"
+            return f"ERREUR 2, Opérateur inconnu ! : {op}"
+        except Exception as e:
+            return f"ERREUR INTERNE: {e}"
+
+    def cmd_dors(self, args):
+        if not args:
+            return "ERREUR 3 : Mauvaise SYNTAXE pour DORS"
+        try:
+            duration = int(args[0])
+            if len(args) > 1:
+                print(" ".join(args[1:]))
+            time.sleep(duration)
+        except ValueError:
+            return "ERREUR 3 : Le temps doit être un entier"
+
+    def cmd_pause(self, args):
+        msg = " ".join(args) if args else "Pause ..."
+        input(msg + "...")
+
+    def cmd_sortir(self, args):
+        self.running = False
+
+    def handle_assignment(self, line):
+        """Handles variable assignment line like ':var name = value'."""
+        try:
+            left_side, _, right_side = line.partition('=')
+            name_parts = left_side.strip().split()
+
+            if name_parts[0].startswith(':'):
+                name_parts[0] = name_parts[0][1:]
+            
+            key = self.normalize_var_name(name_parts)
+            value = right_side.strip()
+            
+            if not value:
+                if key in self.variables:
+                    print(self.variables[key])
+                else:
+                    print("ERREUR 6 VARIABLE N'EXISTE PAS")
             else:
-                input("Pause ...")
-            bon = True
-        if ":" in ligne and not bon:
-            verif = ligne.partition(":")[0]
-            if verif.isspace() or verif == '':
-                try:
-                    verif2 = ligne.partition("=")[2]
-                    if verif2.isspace() or verif2 == '':
-                        namevar = ligne.partition(":")[2].split()
-                        NAME = ''
-                        ende = len(namevar)
-                        for i in range(ende):
-                            NAME += '-' + namevar[i]
-                        try:
-                            print(variables[NAME])
-                        except KeyError:
-                            print("ERREUR 6 VARIABLE N'EXISTE PAS")
-                    else:
-                        namevar = ligne.partition(":")[2].split()
-                        ende = len(namevar)
-                        stop = ende
-                        NAME = ''
-                        STOCK = ''
-                        if namevar[0] == '=':
-                            print("ERREUR 6 NOM DE VARIABLE NON DEFINIE")
-                        else:
-                            for i in range(ende):
-                                if namevar[i] == '=':
-                                    stop = i
-                                    break
-                                NAME += '-' + namevar[i]
-                            for i in range(stop + 1, ende):
-                                if STOCK:
-                                    STOCK += ' ' + namevar[i]
-                                else:
-                                    STOCK = namevar[i]
-                            variables[NAME] = STOCK
-                except:
-                    print("ERREUR 6 VARIABLE N'A PAS DE DEFINITION")
-                bon = True
-        if not bon:
-            if "if" in ligne:
-                v = ligne.partition("if")[2]
-                print("ERREUR 3 : On est pas en Angleterre ici ! Pour si :", " si " + v)
-            elif "print" in ligne or "output" in ligne:
-                key = "print" if "print" in ligne else "output"
-                v = ligne.partition(key)[2]
-                print("ERREUR 3 : On est pas en Angleterre ici ! Pour montrer :", " montre " + v)
-            elif "operat" in ligne:
-                v = ' '.join(ligne.split()[1:])
-                print("ERREUR 3 : On est pas en Angleterre ici ! Pour calcul :", " calcul " + v)
-            elif "exit" in ligne:
-                print("ERREUR 3 : On est pas en Angleterre ici ! Pour sortir :", " sortir")
-            else:
-                print("Uh , je n'arrive pas a déterminer ce que fait cette commande : " + ligne)
+                self.variables[key] = value
+        except Exception:
+            print("ERREUR 6 VARIABLE N'A PAS DE DEFINITION VALIDE")
 
-lignes = []
-try:
-    with open(file, "r") as f:
-        lignes = [line.strip("\n") for line in f.readlines() if line.strip("\n")]
-except:
-    lignes = [file]
+    def execute_line(self, line):
+        line = line.strip()
+        if not line: return
 
-scan(lignes)
+        parts = line.split()
+        cmd = parts[0].lower()
+        args = parts[1:]
+        
+        commands = {
+            "montre": self.cmd_montre,
+            "calcul": lambda a: print(self.cmd_calcul(a)),
+            "si": lambda a: print(self.cmd_si(a)),
+            "dors": self.cmd_dors,
+            "pause": self.cmd_pause,
+            "sortir": self.cmd_sortir
+        }
 
-while True:
-    file = input(">>>")
-    lignes = []
-    try:
-        with open(file, "r") as f:
-            lignes = [line.strip("\n") for line in f.readlines() if line.strip("\n")]
-    except:
-        lignes = [file]
-    scan(lignes)
+        if cmd in commands:
+            commands[cmd](args)
+        elif line.startswith(":"):
+            self.handle_assignment(line)
+        else:
+            if cmd == "print": print("ERREUR 3 : On est pas en Angleterre ici ! Pour montrer : montre ...")
+            elif cmd == "if": print("ERREUR 3 : On est pas en Angleterre ici ! Pour si : si ...")
+            elif cmd == "exit": print("ERREUR 3 : On est pas en Angleterre ici ! Pour sortir : sortir")
+            else: print(f"Commande inconnue : {cmd}")
+
+    def run_file(self, filename):
+        if not os.path.isfile(filename):
+            print("Error, File doesn't exist!")
+            return
+
+        with open(filename, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        for line in lines:
+            if not self.running: break
+            self.execute_line(line)
+
+def main():
+    print("STALINIUM V2 (OPTIMIZED)")
+    interpreter = StaliniumInterpreter()
+    if len(sys.argv) > 1:
+        interpreter.run_file(sys.argv[1])
+    else:
+        while True:
+            try:
+                file = input(">>> ")
+                interpreter.running = True
+                interpreter.run_file(file)
+            except KeyboardInterrupt:
+                print("\nAu revoir, Camarade!")
+                break
+            except Exception as e:
+                print(f"Erreur critique: {e}")
+
+if __name__ == "__main__":
+    main()
